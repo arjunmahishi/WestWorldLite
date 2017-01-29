0a3deca2d0b0dfe8618dcf3ce94951d4464ab407
arjunmahishi <arjun.mahishi@gmail.com>
2017-01-29 11:09:35 +0530

    init world

--- bluePrint.py
+++ bluePrint.py
@@ -2,3 +2,9 @@ class Host:
 	def __init__(self, hostGender, hostName="noname"):
 		self.hostName = hostName
 		self.hostGender = hostGender
+		self.age = 0
+
+class World:
+	def __init__(self):
+		hosts = []
+		
